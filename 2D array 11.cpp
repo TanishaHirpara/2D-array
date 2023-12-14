d0ad84bfde701702ef1f1d315ebbcb8b39ca561b
TanishaHirpara <150216881+TanishaHirpara@users.noreply.github.com>
2023-12-14 10:31:46 +0530

    Add files via upload

--- 2D array 11.cpp
+++ 2D array 11.cpp
@@ -20,7 +20,7 @@ main()
 	{
 		for(j=0;j<5;j++)
 		{
-		if(i==0 || j==0 || i==4 ||j==4)
+		if(i!=0 && i!=4 && j!=0 && j!=4)
 		{
 			sum = sum + a[i][j];
 		}
@@ -30,3 +30,4 @@ main()
 	printf("All sum is = %d " ,sum);
 
 }
+
